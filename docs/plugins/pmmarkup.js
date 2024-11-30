a03a52f0b261b6d57cc7ea8609f58a16c12d9a1d
The 5th Volt <the5thvolt@gmail.com>
2024-11-30 22:36:31 +1100

    fixed the video again

--- docs/plugins/pmmarkup.js
+++ docs/plugins/pmmarkup.js
@@ -38,12 +38,16 @@ function plugin(hook, vm) {
     hook.afterEach(function (html, next) {
         html = html.replace(/%embed%\s*<a.*?href="(https:\/\/youtu\.be\/|https:\/\/www\.youtube\.com\/watch\?v=)([a-zA-Z0-9_-]+)".*?>.*?<\/a>\s*%%/g, function (match, baseUrl, videoId) {
             return `
-            <iframe width="560" height="315" 
-              src="https://www.youtube.com/embed/${videoId}" 
-              frameborder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowfullscreen>
-            </iframe>`;
+            <div class="video">
+              <iframe
+                height=350px
+                src="https://www.youtube.com/embed/${videoId}" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+              </iframe>
+            </div>
+            `;
         });
 
         next(html);
